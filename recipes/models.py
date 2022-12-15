940ebf491fe908647af481b69cd507f43c0ff3ea
Armando Schiano di Cola <armando.schianodicola@gmail.com>
2022-12-15 19:24:44 +0100

    upd

--- recipes/models.py
+++ recipes/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Recipe(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(blank=True)
 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
